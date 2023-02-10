cd676172252ca66c9b5b02fccbfb853e0c13b1ae
Starostin SV <Vlillen@yandex.ru>
2023-02-10 20:21:47 +0400

    Нахождение квадрата целого числа

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число и выдает
+// его квадрат (число умноженное на себя)
+
+Console.WriteLine("Введите число > "); //Ввод в консоль приглашения для ввода
+String inputValue;                      //Объявление переменной, в которую будем сохранять
+inputValue = Console.ReadLine(); //Присвоение переменной из консоли
+int value = Convert.ToInt32(inputValue);
+
+int square = value * value; // Вычисление квадрата
+
+//вывод на экран
+
+System.Console.WriteLine("Квадрат числа " + value +  " равен " + square);
+System.Console.WriteLine($"Квадрат числа {value} равен  {square}");
