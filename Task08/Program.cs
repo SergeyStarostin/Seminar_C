06b97ac383cb1d460535d997dacc3e8142afb50f
Starostin SV <Vlillen@yandex.ru>
2023-02-14 22:39:16 +0400

    Ппрограмма, которая принимает на вход число и проверяет, кратно ли оно одновременно

---
+++ Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = Prompt("Введите число ");
+int divider1 = Prompt("Введите делитель 1 ");
+int divider2 = Prompt("Введите делитель 2 ");
+
+if (num % divider1 == 0 && num % divider2 == 0)
+{
+    System.Console.WriteLine($"Число {num} кратно одновременно {divider1} и {divider2}");
+}
+else
+{
+    System.Console.WriteLine($"Число {num} не кратно одновременно {divider1} и {divider2}");
+}
