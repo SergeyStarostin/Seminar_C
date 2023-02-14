ebe102f10479b8c1838f49d70649ab156cf18246
Starostin SV <Vlillen@yandex.ru>
2023-02-14 22:03:59 +0400

    Решение с помощью массива

---
+++ Task05/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Validate(int num)
+{
+    if (num >= 100 && num < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Это число не трехзначное");
+    return false;
+}
+
+int number = Prompt("Введите 3-хзначное число");
+if (Validate(number))
+{
+    int lastDigit = number % 10;
+    System.Console.WriteLine($"последняя цифра числа {number} равна {lastDigit}");
+}
