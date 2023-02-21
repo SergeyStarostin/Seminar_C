80c869a8f68a7220ebb363b52c2cf6f307ab16bd
Starostin SV <Vlillen@yandex.ru>
2023-02-21 21:28:15 +0400

    решение с помощью формулы гауса

---
+++ Task05.1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// //вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int Number = Prompt("Введите трехзначное число: ");
+
+if (Number > 99 && Number < 1000)
+{
+    int SecondNumber = Number % 100 / 10;
+    System.Console.Write($"Вторая цифра трехзначного числа = {SecondNumber}");
+}
+else
+{
+    System.Console.Write($"{Number} не является трехзначным");
+}
---
+++ Task14/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А) и 
+// выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// Ввод числа
+int InputInt(string prompt)
+{
+    System.Console.Write($"{prompt} >");            // Вывод приглашения на экран
+    return Convert.ToInt32(Console.ReadLine());     // Ввод с консоли
+}
+
+// Проверяет введенное число, что оно больше 0
+bool ValidatePositive(int value)
+{
+    if (value <= 0)     // Сама проверка
+    {
+        System.Console.WriteLine("Введите положительное число");    // Выводим ошибку пользователю
+        return false;       // Признак неправильрного ввода
+    }
+
+    return true;            // Можно работать дальше
+}
+
+// Подсчет сумма от 1 до value
+int SumNumbers(int value)
+{
+    int sum = 0;    // Будем здесь накапливать сумму
+    for(int i = 1; i <= value; i++) //Цикл вычисления суммы
+    {
+        sum += i;                   // Накапливаем сумму
+    }
+    return sum;
+}
+
+// Подсчет суммы Гауссом
+int SumGaussNumbers(int value)
+{
+    return (1+value)*value/2;
+}
+
+int value = InputInt("Введите число");  // Ввод числа
+if (ValidatePositive(value))        // Проврека на положительное число
+{
+    int sum = SumNumbers(value);
+    System.Console.WriteLine($"Сумма чисел от 1 до {value} равна {sum}");
+    System.Console.WriteLine($"Сумма чисел по Гауссу от 1 до {value} равна {SumGaussNumbers(value)}");
+}
