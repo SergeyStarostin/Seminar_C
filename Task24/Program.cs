6d24383b6ed8de149b6b6cdfde999d00db99bef4
Starostin SV <Vlillen@yandex.ru>
2023-03-03 22:34:16 +0400

    Задать двумерный массив. Найдите элементы, у которых обе позиции чётные, и замените эти элементы на их квадраты

---
+++ Task24/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 2: Задайте двумерный массив. Найдите элементы, у которых обе позиции чётные, и замените эти элементы на их квадраты.
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col]; // Создаем 2-мерный массив
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(2, 20);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+int[,] ModifaryArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 2)
+        {
+            array[i, j] *= array[i, j];
+        }
+    }
+    return array;
+}
+
+int[,] matrix = GenerateArray(3, 4);
+PrintArray(matrix);
+PrintArray(ModifaryArray(matrix));
