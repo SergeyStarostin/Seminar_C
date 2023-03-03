e95f87852579b342a6df5d8f4cb01353667a595b
Starostin SV <Vlillen@yandex.ru>
2023-03-03 22:47:49 +0400

    Нахождение элементов матрицы по главной диаганали

---
+++ Task25/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+// Например, задан массив:
+// 1   4   7
+// 5   9   2
+// 8   4   2
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col]; // Создаем 2-мерный массив
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(2, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+int SumArray(int[,] array)
+{
+    int result = 0;
+    for (int k = 0; k < array.GetLength(0); k++)
+    {
+        result += array[k, k];
+    }
+    return result;
+}
+
+
+int[,] matrix = GenerateArray(4, 4);
+PrintArray(matrix);
+Console.WriteLine($"Сумма элементов по диагонали = {SumArray(matrix)}");
