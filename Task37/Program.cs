154d59034fbd48b0c1ec50e0cc97a6e893211976
Starostin SV <Vlillen@yandex.ru>
2023-03-10 23:50:31 +0400

    Программа, которая проверяет, является ли число простым

---
+++ Task37/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Проверка на простое число: (Простое число делится на себя и на 1)
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+bool CheckPrime(long num, long div)
+{
+    if (div == 1) return true;
+    return num % div != 0 && CheckPrime(num, div - 1);
+}
+
+int number = 31, div = number - 1;
+Console.WriteLine($"{CheckPrime(number, div)}");
