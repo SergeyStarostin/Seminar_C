5a3fad77ff5a234223bc85e7869b7487c94509b2
Starostin SV <Vlillen@yandex.ru>
2023-03-10 23:28:42 +0400

    Программа, которая проверяет, является ли число степенью от 2

---
+++ Task36/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 5: Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+bool CheckBin(int num)
+{
+    if (num == 1) return true;
+    return num % 2 == 0 && CheckBin(num / 2);
+}
+
+int number = 32;
+Console.WriteLine($"{CheckBin(number)}");
