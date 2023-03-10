ebdd94d75a12a5262a689912d6213491cc1c8b11
Starostin SV <Vlillen@yandex.ru>
2023-03-10 22:53:42 +0400

    Задайте значения M и N. Программа, которая рекурсивно выведет все натуральные числа в промежутке от M до N.

---
+++ Task33/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая рекурсивно выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+void ShowNumber(int first, int second)
+{
+    if (first > second) return;
+    Console.Write($"{first}  ");
+    ShowNumber(first + 1, second);
+}
+
+int num = 4, numTwo = 8;
+ShowNumber(num, numTwo);
