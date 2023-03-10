033098d103a1ee7906fbcd07b0e0110774f1d56a
Starostin SV <Vlillen@yandex.ru>
2023-03-10 22:44:19 +0400

    Программа, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии

---
+++ Task32/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void ShowNumbers(int number)
+{
+    if (number<=0)
+    {
+        return;
+    }
+System.Console.WriteLine($"{number}");
+ShowNumbers(number - 1);
+System.Console.WriteLine($"{number}");
+}
+int num = 6;
+ShowNumbers(num);
