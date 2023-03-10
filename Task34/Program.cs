3154b17cb483476562a0b9c6fc1248c4c13fab84
Starostin SV <Vlillen@yandex.ru>
2023-03-10 23:07:55 +0400

    Программа, которая будет принимать на вход число и возвращать сумму его цифр. Использовать рекурсию.

---
+++ Task34/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int SumNumbers(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumNumbers(num / 10);
+}
+
+int number = 19236;
+Console.WriteLine($"{SumNumbers(number)}");
